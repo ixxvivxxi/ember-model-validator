e398bec5e14bf3379943a5a43c459a88a3548a32
Anton <ixxvivxxi@yahoo.com>
2025-09-19 14:00:15 +0300

    feat: allow to validate async: false relations

--- addon/decorators/core-validator.js
+++ addon/decorators/core-validator.js
@@ -123,8 +123,13 @@ function coreValidator(constructor) {
       let propertyValue = get(this, property);
       // If the property is an async relationship.
       if (this._modelRelations() && !isBlank(this._modelRelations()[property])) {
-        propertyValue = get(this, `${property}.content`);
+        if (this['relationshipFor'](property).options?.async === false) {
+          propertyValue = this._modelRelations()[property]
+        } else {
+          propertyValue = get(this, `${property}.content`);
+        }
       }
+
       if (isBlank(propertyValue)) {
         set(this, 'isValidNow', false);
         this._addToErrors(property, validation.presence, this._validationMessages.presenceMessage);
@@ -625,8 +630,14 @@ function coreValidator(constructor) {
       } else {
         const relationships = {};
         if (this.constructor.eachRelationship) {
-          this.constructor.eachRelationship((name) => {
-            relationships[name] = this['relationshipFor'](name);
+          this.constructor.eachRelationship((name, descriptor) => {
+            if (descriptor.options?.async === false) {
+              if (get(this, `${name}.id`)) {
+                relationships[name] = {content: { id: get(this, `${name}.id`) }}
+              }
+            } else {
+              relationships[name] = this['relationshipFor'](name);
+            }
           });
         }
         return relationships;
